09375bc15918f711ed42849000e3deabc8a8e6e2
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-07 11:42:09 +0000

    Returning HTML

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+  "Hello"
+end
+
+get '/secret' do
+  "Hello Dania"
+end
+
+get '/cat' do
+  '<img src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png">'
+end
