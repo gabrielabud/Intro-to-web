1efa6333ae7a2b4a6384cd50aad604eac298a18c
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-07 15:15:24 +0000

    Form added

--- app.rb
+++ app.rb
@@ -4,10 +4,20 @@ get '/' do
   "Hello"
 end
 
-get '/secret' do
-  "Hello Marie"
+get '/random-cat' do
+  @name = ["Amigo","Oscar","Viking"].sample
+  erb(:named_cat)
+  #erb reads the input file, proccesses ruby that is inside and returns resulting
+  #HTML. this HTML is then returned by the block passed to the get() method
 end
 
-get '/cat' do
-  erb(:index)
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  erb(:named_cat)
+end
+
+get '/form' do
+  erb(:cat_form)
 end
